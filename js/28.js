405ffb26461890236bf3208a292d759ce297bb75
Brayan Albadam <brayanalbadam@gmail.com>
2024-03-22 17:00:10 -0500

    Javascript

--- js/28.js
+++ js/28.js
@@ -7,6 +7,8 @@ const heading = document.querySelector('.heading')
 // console.log(heading.classList)
 // console.log(heading.id)
 
+//El DOM son los atributos que tiene un elemento, pero tambien
+//como esta conectado el HTML
 
 const enlaces = document.querySelector('.navegacion a')
 const enlaces2 = document.querySelectorAll('.navegacion a')
