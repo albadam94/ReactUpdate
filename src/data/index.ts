3804f49bae7395d93b7144b7b8f7f78473840ede
Brayan Albadam <brayanalbadam@gmail.com>
2024-07-11 18:31:24 -0500

    zod

---
+++ src/data/index.ts
@@ -0,0 +1,8 @@
+import {Currency} from "../types";
+
+export const currencies:Currency[] =[
+    {code:'USD',name:'Dolar Americano'},
+    {code:'EUR',name:'Euro'},
+    {code:'GBP',name:'Libra Esterlina'},
+    {code:'COP',name:'Peso Colombiano'}
+]
