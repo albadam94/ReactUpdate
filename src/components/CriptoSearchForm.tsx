58c1ec54e947b73b558d60d86114a1a029492f5b
Brayan Albadam <brayanalbadam@gmail.com>
2024-07-12 16:52:34 -0500

    zod

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,10 +1,27 @@
 import{currencies} from'../data'
+import { useCryptoStore } from '../store'
+import {ChangeEvent,useState} from "react"
+import{Pair} from '../types'
+
+
 export default function CriptoSearchForm() {
+  const cryptocurrencies=useCryptoStore((state)=>state.cryptocurrencies)
+  const[pair,setPair]=useState<Pair> ({
+    currency:'',
+    criptocurrency:''
+  })
+
+  const handleChange=(e:ChangeEvent<HTMLSelectElement> )=>{
+    setPair({
+      ...pair,
+      [e.target.name]:e.target.value
+    })
+  }
   return (
     <form className="form">
         <div className="field">
             <label htmlFor="currency">Moneda:</label>
-            <select  name="currency" id="currency">
+            <select  name="currency" id="currency" onChange={handleChange}>
                 <option value="">--Seleccione--</option>
                 {currencies.map(currency=>(
                 <option key={currency.code} value={ currency.code}>{currency.name}</option>
@@ -14,8 +31,14 @@ export default function CriptoSearchForm() {
 
         <div className="field">
             <label htmlFor="criptocurrency">Criptomoneda:</label>
-            <select  name="criptocurrency" id="criptocurrency">
+            <select  name="criptocurrency" id="criptocurrency" onChange={handleChange}>
             <option value="">--Seleccione--</option>
+            {cryptocurrencies.map(crypto=>(
+                <option
+                  key={crypto.CoinInfo.Fullname}
+                  value={crypto.CoinInfo.Name}
+                >{crypto.CoinInfo.Fullname}</option>
+            ))}
               
             </select>
         </div>
--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -5,3 +5,17 @@ export const CurrencySchema=z.object({
     code:z.string(),
     name:z.string()
 })
+
+export const CryptoCurrencyResponseSchema = z.object({
+        CoinInfo:z.object({
+            Fullname:z.string(),
+            Name:z.string()
+    })
+})
+
+export const CryptoCurrenciesResponseSchema=z.array(CryptoCurrencyResponseSchema)
+
+export const PairSchema=z.object({
+    currency:z.string(),
+    criptocurrency:z.string()
+})
