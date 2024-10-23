ab3eebc422a2215aac4495d1f7d849db2a073d98
Brayan Albadam <brayanalbadam@gmail.com>
2024-10-23 18:28:46 -0500

    api

--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import {CryptoCurrenciesResponseSchema} from '../schema/crypto-schema'
+import{Pair} from '../types'
 
 export async function getCryptos(){
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
@@ -12,5 +13,7 @@ export async function getCryptos(){
 }
 
 export async function fetchCurrentCryptoPrice(pair: Pair) {
-    console.log (pair)
+    const url= `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.criptocurrency}&tsyms=${pair.currency}`
+    const {data: {DISPLAY}} = await axios(url)
+    console.log (DISPLAY[pair.criptocurrency][pair.currency])
 }
